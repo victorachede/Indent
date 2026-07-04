9dc3a61a5ec9030c084e8c3e9706ee3ee054de55
Victor Achede <colepalmerx6@gmail.com>
2026-07-04 11:17:09 -0800

    fix: use forward slashes in mv paths for bash on Windows

--- build/windows/prepare_assets.sh
+++ build/windows/prepare_assets.sh
@@ -32,22 +32,22 @@ cd ..
 
 if [[ "${SHOULD_BUILD_EXE_SYS}" != "no" ]]; then
   echo "Moving System EXE"
-  mv "vscode\\.build\\win32-${VSCODE_ARCH}\\system-setup\\VSCodeSetup.exe" "assets\\${APP_NAME}Setup-${VSCODE_ARCH}-${RELEASE_VERSION}.exe"
+  mv "vscode/.build/win32-${VSCODE_ARCH}/system-setup/VSCodeSetup.exe" "assets/${APP_NAME}Setup-${VSCODE_ARCH}-${RELEASE_VERSION}.exe"
 fi
 
 if [[ "${SHOULD_BUILD_EXE_USR}" != "no" ]]; then
   echo "Moving User EXE"
-  mv "vscode\\.build\\win32-${VSCODE_ARCH}\\user-setup\\VSCodeSetup.exe" "assets\\${APP_NAME}UserSetup-${VSCODE_ARCH}-${RELEASE_VERSION}.exe"
+  mv "vscode/.build/win32-${VSCODE_ARCH}/user-setup/VSCodeSetup.exe" "assets/${APP_NAME}UserSetup-${VSCODE_ARCH}-${RELEASE_VERSION}.exe"
 fi
 
 if [[ "${VSCODE_ARCH}" == "ia32" || "${VSCODE_ARCH}" == "x64" ]]; then
   if [[ "${SHOULD_BUILD_MSI}" != "no" ]]; then
     echo "Moving MSI"
-    mv "build\\windows\\msi\\releasedir\\${APP_NAME}-${VSCODE_ARCH}-${RELEASE_VERSION}.msi" assets/
+    mv "build/windows/msi/releasedir/${APP_NAME}-${VSCODE_ARCH}-${RELEASE_VERSION}.msi" assets/
   fi
 
   if [[ "${SHOULD_BUILD_MSI_NOUP}" != "no" ]]; then
     echo "Moving MSI with disabled updates"
-    mv "build\\windows\\msi\\releasedir\\${APP_NAME}-${VSCODE_ARCH}-updates-disabled-${RELEASE_VERSION}.msi" assets/
+    mv "build/windows/msi/releasedir/${APP_NAME}-${VSCODE_ARCH}-updates-disabled-${RELEASE_VERSION}.msi" assets/
   fi
 fi
